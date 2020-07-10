62c408f0f1f0f5b9a57db48f2dfe6d490e812f6e
tianshaoqing <tsq@ddd.com>
2020-07-10 10:17:55 +0800

    day1 实现http handler

--- geew.go
+++ geew.go
@@ -1,2 +1,49 @@
 package geew
 
+import (
+	"fmt"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by geew
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New is the constructor of geew.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	e.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start up a http server
+func (e *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, e)
+}
+
+// Implement the HTTP ServeHTTP
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := e.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
